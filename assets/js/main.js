b60d376819bd4878156fc9e7d78cfcbb42080845
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-06-10 21:48:10 -0300

    finished testimonials section

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,6 +93,26 @@ modalCloses.forEach((modalClose, index) => {
 });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiper = new Swiper('.testimonials__container', {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
 
 /*=============== INPUT ANIMATION ===============*/
 
